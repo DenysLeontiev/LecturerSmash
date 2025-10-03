8a3e91c6f3eef26c9e623d43ef7ac94fc2d3f5b5
Denys Leon <leonden0046@gmail.com>
2025-10-03 17:20:36 +0300

    Added endpoints

--- TeachersRating.API/Program.cs
+++ TeachersRating.API/Program.cs
@@ -10,6 +10,9 @@ builder.Services.AddOpenApi();
 
 builder.Services.RegisterDbContext(builder.Configuration);
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
 using (var scope = app.Services.CreateScope())
@@ -19,7 +22,7 @@ using (var scope = app.Services.CreateScope())
     {
         var context = services.GetRequiredService<AppDbContext>();
         context.Database.Migrate();
-        DataBaseSeeded.SeedDataAsync(context).Wait();
+        // DataBaseSeeded.SeedDataAsync(context).Wait();
     }
     catch (Exception ex)
     {
@@ -33,6 +36,42 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
+app.UseSwagger();
+app.UseSwaggerUI();
+
 app.UseHttpsRedirection();
+
+app.MapGet("api/institutes", async (AppDbContext context) =>
+{
+    var institutes = await context.Institutes.AsNoTracking()
+        .ToListAsync();
+
+    var mappedInstitutes = institutes.Select(i => i.ToDto()).ToList();
+
+    return Results.Ok(mappedInstitutes);
+}).WithName("GetInstitutes");
+
+app.MapGet("api/departments/{intstituteId:guid}", async (Guid intstituteId, AppDbContext context) =>
+{
+    var departments = await context.Departments.Where(x => x.InstituteId.Equals(intstituteId))
+        .AsNoTracking()
+        .ToListAsync();
+
+    var mappedDepartments = departments.Select(d => d.ToDto()).ToList();
+
+    return Results.Ok(mappedDepartments);
+}).WithName("GetDepartmentsForInstitute");
+
+app.MapGet("api/workers/{departmentId:guid}", async (Guid departmentId, AppDbContext context) =>
+{
+    var workers = await context.Workers.Where(w => w.Departments.Any(d => d.Id == departmentId))
+        .Include(w => w.Photo)
+        .AsNoTracking()
+        .ToListAsync();
+
+    var mappedWorkers = workers.Select(w => w.ToDto()).ToList();
+
+    return Results.Ok(mappedWorkers);
+}).WithName("GetWorkersForDepartment");
 
 app.Run();
