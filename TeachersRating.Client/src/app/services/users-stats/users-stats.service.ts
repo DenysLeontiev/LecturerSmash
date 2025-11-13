244c43413c502108c7a988f537d51de6543f603e
Denys Leon <leonden0046@gmail.com>
2025-11-13 23:51:10 +0200

    including todays date

--- TeachersRating.Client/src/app/services/users-stats/users-stats.service.ts
+++ TeachersRating.Client/src/app/services/users-stats/users-stats.service.ts
@@ -12,9 +12,15 @@ export class UsersStatsService {
   private baseUrl = environment.baseUrl;
 
   getStatsByDateRange(dateStart: Date, dateEnd: Date): Observable<UsersStats[]> {
+    const startDate = new Date(dateStart);
+    startDate.setHours(0, 0, 0, 0);
+    
+    const endDate = new Date(dateEnd);
+    endDate.setHours(23, 59, 59, 999);
+    
     const params = {
-      dateStart: dateStart.toISOString(),
-      dateEnd: dateEnd.toISOString()
+      dateStart: startDate.toISOString(),
+      dateEnd: endDate.toISOString()
     };
     return this.http.get<UsersStats[]>(`${this.baseUrl}/stats/days`, { params });
   }
